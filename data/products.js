77a687726556a58532e3b9f91a9596c118ccfb53
kanokwan-st <ganokwn123@gmail.com>
2025-04-07 23:31:25 +0700

    feat(files): create component ProductCard and data products

---
+++ data/products.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        image: '/public/assets/productPicture/Abstract-Painting-Modern-Art-1.jpg',
+        title: 'Modern Portrait in Brown',
+        artist: 'By Sophia Laurent',
+        price: '$2,450.00'
+    },
+    {
+        image: '/public/assets/productPicture/Abstract-Painting-Classic-Art-5.jpg',
+        title: 'Faint Shades of Silence',
+        artist: 'By Marcus Aurelio',
+        price: '$3,870.00'
+    },
+    {
+        image: '/public/assets/productPicture/Portrait-Painting-Modern-Art-2.jpg',
+        title: 'Grace in Elegance',
+        artist: 'By Isabella Moretti',
+        price: '$5,620.00'
+    },
+    {
+        image: '/public/assets/productPicture/Historical-Painting-Classic-Art-2.jpg',
+        title: 'Wisdom of the Ages',
+        artist: 'By Pietro Bernini',
+        price: '$6,350.00'
+    },
+]
+
+export default products;
