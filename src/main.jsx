5d94803df6c35cc909718768ce6a87131f70e4c8
kanokwan-st <ganokwn123@gmail.com>
2025-04-09 23:21:54 +0700

    feat(main, Layout,App): Add Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createTheme ,  ThemeProvider } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Layout from "./Layout.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import LandingPage from "./pages/LandingPage.jsx"
 
 const theme = createTheme({
   palette: {
@@ -9,7 +15,7 @@ const theme = createTheme({
       main: "#62483a",
       text: "#f2eee7",
       hoverText: "#49352a",
-      bgButton:"#62483a3b",
+      bgButton: "#62483a3b",
       cream: "#f0e0d0",
       darkCream: "#e9e2d6",
       fontGray: "#757575",
@@ -25,17 +31,35 @@ const theme = createTheme({
     },
     secondary: {
       main: "#806248",
-    }
+    },
   },
   typography: {
     primary: {
       main: "Arial, Helvetica, sans-serif",
-    }
-  }
+    },
+  },
 });
+
+
+// ------------- ROUTER ----------------------
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <LandingPage /> },
+      { path: "/register", element: <Register /> },
+      { path: "/login", element: <Login /> },
+      // ADD PATH HERE
+    ],
+  },
+]);
+// -------------------------------------------
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider theme={theme}>
-    <App />
+    <App>
+      <RouterProvider router={router} />
+    </App>
   </ThemeProvider>
 );
