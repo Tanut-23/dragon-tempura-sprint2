044340feb2350fce26fdda5543a29a876d562c7f
Nutty <tanut.ktr@gmail.com>
2025-05-06 14:30:17 +0700

    deleted nav layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,20 +5,6 @@ import { Link, Outlet } from "react-router-dom";
 function Layout() {
   return (
     <div className="flex flex-col w-full min-h-screen items-center bg-amber-100">
-      <nav className="flex gap-10 flex-wrap my-20 text-2xl">
-        <Link to="/">Landing</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/mainshop">Main Shop </Link>
-        <Link to="/myorder">My order</Link>
-        <Link to="/auction">Auction</Link>
-
-        <Link to="/resetpassword">Reset password</Link>
-        <Link to="/cart">Matetest</Link>
-        <Link to="/market">Market</Link>
-        <Link to="/postpage">PostPage</Link>
-        <Link to="/animation">Animation</Link>
-      </nav>
       <div className="w-full sticky top-0 z-50 ">
         <Navbar />
       </div>
